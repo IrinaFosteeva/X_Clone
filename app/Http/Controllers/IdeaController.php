57af083b1cd5cee639a3c7903da42ec2c7db34de
IrinaF <ira523523523@gmail.com>
2024-03-11 20:04:03 +0300

    view a single idea

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -26,10 +26,21 @@ class IdeaController extends Controller
 
     }
 
-    public function destroy($id) {
-        Idea::where('id', $id)->firstOrFail()->delete();
+    public function destroy(Idea $idea) {
+        //Idea::where('id', $id)->firstOrFail()->delete();
+        $idea->delete();
 
         return redirect()->route('dashboard')->with('success1', 'Idea deleted successfully!');
 
     }
+
+    public function show(Idea $idea) {
+
+//        return view('ideas.show', [
+//            'idea' => $idea
+//        ]);
+
+        return view('ideas.show', compact('idea'));
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ use App\Http\Controllers\ProfileController;
 */
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
 Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.create');
-Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 Route::get('/terms', function () {
     return view('terms');
